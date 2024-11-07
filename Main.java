e45b4b1d05f4846b71bb53ca9161edf6a94bd138
antivsan796 <antivsan@espol.edu.ec>
2024-11-06 22:44:54 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,5 +2,17 @@ public class Main{
     public static void main(String[] args) {
         ReportCreator reportCreator = ReportCreator.getInstance();
         // ACÁ VAN METODOS, TRABAJEN ESTO XD
+        
+        ReportFactory pdfFactory = new PDFReportFactory();
+        Report pdfReport = pdfFactory.createReport();
+        pdfReport.generateContent();
+
+        ReportFactory excelFactory = new ExcelReportFactory();
+        Report excelReport = excelFactory.createReport();
+        excelReport.generateContent();
+
+        ReportFactory wordFactory = new WordReportFactory();
+        Report wordReport = wordFactory.createReport();
+        wordReport.generateContent();
     }
 }
